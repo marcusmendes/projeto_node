5805bd1f067c0daee61ed29ad13a3efc1d15231f
Marcus Maciel <marcusmaciel@controllab>
2019-10-15 15:37:09 -0300

    API de categorias

--- src/lib/lomadee/models/Category.js
+++ src/lib/lomadee/models/Category.js
@@ -6,7 +6,43 @@ import Lomadee from '../Lomadee';
  * @see https://developer.lomadee.com/afiliados/ofertas/recursos-v3/buscar-categorias/
  */
 class Category extends Lomadee {
+  /**
+   * Busca por categorias através de palavras.
+   *
+   * @param {string} keyword    Palavra-chave
+   * @param {integer} storeId   ID de loja.
+   * @param {integer} hasOffer  true|false. Quando "true" retorna apenas
+   *                            categorias que possuem ofertas.
+   */
+  search(keyword, storeId = '', hasOffer = true) {
+    return this.get('/category/_search', {
+      keyword,
+      storeId,
+      hasOffer,
+      size: this.itemsPerPage,
+    });
+  }
 
+  /**
+   * Retorna a lista de todas as categorias que possuem ofertas
+   *
+   * @param {integer} storeId   ID de loja.
+   * @param {boolean} hasOffer  true|false. Quando "true" retorna apenas
+   *                            categorias que possuem ofertas.
+   */
+  getCategories(storeId = '', hasOffer = true) {
+    return this.get('/category/_all', { storeId, hasOffer });
+  }
+
+  /**
+   * Busca uma categoria específica pelo seu ID
+   *
+   * @param {integer} categoryId ID da categoria.
+   * @param {integer} storeId    ID de loja.
+   */
+  getCategory(categoryId, storeId = '') {
+    return this.get(`/category/_id/${categoryId}`, { storeId });
+  }
 }
 
 export default new Category();
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,11 @@ import { Router } from 'express';
 
 import DefaultController from './controllers/DefaultController';
 import OfferController from './controllers/OfferController';
+import CategoryController from './controllers/CategoryController';
+
+/* Middlewares */
+import offerSearchValidaton from './validations/Offer/searchValidation';
+import categorySearchValidation from './validations/Category/searchValidation';
 
 const routes = new Router();
 
@@ -10,9 +15,14 @@ routes.get('/', DefaultController.index);
 
 /* Offers */
 routes.get('/offers', OfferController.index);
+routes.get('/offers/search', offerSearchValidaton, OfferController.search);
 routes.get('/offers/:offerId', OfferController.offer);
-routes.get('/offers/search', OfferController.search);
 routes.get('/offers/category/:categoryId', OfferController.offersOfCategory);
 routes.get('/offers/store/:storeId', OfferController.offersOfStore);
+
+/* Category */
+routes.get('/categories', CategoryController.all);
+routes.get('/categories/search', categorySearchValidation, CategoryController.search);
+routes.get('/categories/:categoryId', CategoryController.category);
 
 export default routes;
